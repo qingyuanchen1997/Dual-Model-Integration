9909deee205ff547af658f4433fb0f1755ac0000
qingyuanchen1997 <53213889+qingyuanchen1997@users.noreply.github.com>
2022-04-24 21:16:30 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-python code/fits2jpg.py
-python code/main_sunspot2_c.py
-python code/main_sunspot2_m.py
-python code/main_sunspot2.py
-python code/inference_test_alpha.py
-python code/train-betax.py
-python code/inference_test_betax.py
-python code/TXT.py
